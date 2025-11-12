96ec9e7fc0bd85e0ddd81600d551f3b4128916ab
George Anaschenkov <aga@altx-soft.ru>
2025-11-12 17:50:03 +0300

    Refine `DuplicateZeros1089`

--- LeetCodeProblemsLibrary/1089_Duplicate_Zeros.cs
+++ LeetCodeProblemsLibrary/1089_Duplicate_Zeros.cs
@@ -1,24 +1,23 @@
-namespace LeetCodeProblemsLibrary
-{
-    public static class DuplicateZeros1089 {
-        public static void DuplicateZeros(int[] arr) {
-            if (arr.Length == 1) 
-                return;
+namespace LeetCodeProblemsLibrary;
 
-            int i = 0;
-            int next = arr[1];
-            while (i < arr.Length - 1)
+public static class DuplicateZeros1089 {
+    public static void DuplicateZeros(int[] arr) {
+        if (arr.Length == 1) 
+            return;
+
+        int i = 0;
+        int next = arr[1];
+        while (i < arr.Length - 1)
+        {
+            if (next == 0)
             {
-                if (next == 0)
-                {
-                    next = arr[i + 1];
-                    arr[i + 1] = 0;
-                    i+=2;
-                }
-                
-                (arr[i], next) = (next, arr[i]);
-                i++;
+                next = arr[i + 1];
+                arr[i + 1] = 0;
+                i+=2;
             }
+                
+            (arr[i], next) = (next, arr[i]);
+            i++;
         }
     }
 }
