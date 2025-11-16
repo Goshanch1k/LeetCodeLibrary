f2924279cad88dda3ffc79051ad785eb7ccfc528
George Anaschenkov <aga@altx-soft.ru>
2025-11-16 15:53:48 +0300

    Solution of `NumSub1513`

---
+++ LeetCodeProblemsLibrary/Medium/1513_Number_of_Substrings_With_Only_1s.cs
@@ -0,0 +1,27 @@
+namespace LeetCodeProblemsLibrary.Medium;
+
+public static class NumSub1513 {
+    public static int NumSub(string s) {
+        double result = 0;
+        const int massiveNumber = 1000000007;
+        
+        double counter = 0;
+        int i = 0;
+        while (i < s.Length)
+        {
+            while (i < s.Length && s[i] == '1')
+            {
+                counter++;
+                i++;
+            }
+            
+            result = (result + counter * (counter + 1) / 2) % massiveNumber;
+            counter = 0;
+
+            i++;
+        }
+        
+        return (int)result;
+    }
+    
+}
